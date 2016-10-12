afee441b0e91c7148cd86d7cf8532fc64cef99d9
ABShubin <abshubin@users.noreply.github.com>
2016-10-12 11:12:31 -0700

    Wrote test in CountdownTest.java

--- tst/CountdownTest.java
+++ tst/CountdownTest.java
@@ -1,6 +1,7 @@
 import org.testng.annotations.Test;
-
 import static org.testng.Assert.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 /**
  * Created by Andrew Shubin on 10/10/16.
@@ -15,6 +16,19 @@ public class CountdownTest {
          * At least one must pass.
          * At least one must fail.
          */
+        
+        // Arrange
+        int testNum = 10;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        String expected = "10\n9\n8\n7\n6\n5\n4\n3\n2\n1\nGO!";
+        
+        // Act
+        Countdown.count(testNum);
+        String result = out.toString();
+        
+        // Assert
+        assertEquals(result, expected, "ERROR in Countdown.count()");
     }
 
 }
