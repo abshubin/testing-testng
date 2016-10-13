3e8f54a88653c189535e288b7d4bd53b9f6224f9
Shubin, Andrew <andrewbshubin@gmail.com>
2016-10-12 17:47:26 -0700

    Final Commit

--- tst/CountdownTest.java
+++ tst/CountdownTest.java
@@ -9,20 +9,106 @@ import java.io.PrintStream;
 public class CountdownTest {
 
     @Test
-    public void testCount() throws Exception {
+    public void testCount_happy() throws Exception {
         
         // Arrange
         int testNum = 10;
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
-        String expected = "10\n9\n8\n7\n6\n5\n4\n3\n2\n1\nGO!";
+        String expected = fauxCount(testNum);
         
         // Act
+        out.reset();
         Countdown.count(testNum);
         String result = out.toString();
         
         // Assert
-        assertEquals(result, expected, "ERROR in Countdown.count()");
+        assertEquals(result, expected, "ERROR in Countdown.count() -- TESTING input of " + testNum);
+    }
+
+    @Test
+    public void testCount_high() throws Exception {
+
+        // Arrange
+        int testNum = 10000;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        String expected = fauxCount(testNum);
+
+        // Act
+        out.reset();
+        Countdown.count(testNum);
+        String result = out.toString();
+
+        // Assert
+        assertEquals(result, expected, "ERROR in Countdown.count() -- TESTING input of " + testNum);
+    }
+
+    @Test
+    public void testCount_small() throws Exception {
+
+        // Arrange
+        int testNum = 1;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        String expected = fauxCount(testNum);
+
+        // Act
+        out.reset();
+        Countdown.count(testNum);
+        String result = out.toString();
+
+        // Assert
+        assertEquals(result, expected, "ERROR in Countdown.count() -- TESTING input of " + testNum);
+    }
+
+    @Test
+    public void testCount_zero() throws Exception {
+
+        // Arrange
+        int testNum = 0;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        String expected = fauxCount(testNum);
+
+        // Act
+        out.reset();
+        Countdown.count(testNum);
+        String result = out.toString();
+
+        // Assert
+        assertEquals(result, expected, "ERROR in Countdown.count() -- TESTING input of " + testNum);
+    }
+
+    @Test
+    public void testCount_negative() throws Exception {
+
+        // Arrange
+        int testNum = -10;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        String expected = fauxCount(testNum);
+
+        // Act
+        out.reset();
+        Countdown.count(testNum);
+        String result = out.toString();
+
+        // Assert
+        assertEquals(result, expected, "ERROR in Countdown.count() -- TESTING input of " + testNum);
+    }
+
+    private static String fauxCount(int testNum) {
+
+        String output = "";
+
+        for (int i = testNum; i > 0; i--) {
+            output += (i + "\n");
+        }
+
+        output += "GO!\n";
+
+        return output;
     }
 
 }
